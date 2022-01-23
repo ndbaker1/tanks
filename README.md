a54a3f2c711dcd7c8c2355d933735a85aafcb349
Nick Baker <ndbaker1@outlook.com>
2022-01-22 21:49:54 -0600

    - added pack-wasm script for creating dist folder - change package names to use '_' instead of '-'

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 🎮 A Realtime Multiplayer Server/Client Game example built entirely with Rust 🦀
 
-✨Zero lines of JavaScript besides [html file for bootstrapping wasm](./tanks-wasm/index.html)✨
+✨Zero lines of JavaScript besides [html file for bootstrapping wasm](./tanks_wasm/index.html)✨
 
 ## Background
 
@@ -15,3 +15,15 @@ Therefore, I decided to make this as a general example for myself (and practice 
 The server is a [`Warp`](https://github.com/seanmonstar/warp) server setup to handle WebSockets.
 <br>
 I wrote [this websocket server](https://github.com/ndbaker1/websocket-server) wrapper that accepts custom `MessageEventHandler` and `ServerTickHandler`, that way I can reuse it for multiple browser games.
+
+## Building 🔨
+
+`pack-wasm ` lib is my hacky way of creating a custom build script for packing wasm and including an index page file which I find slightly annoying using `wasm-pack` because you cant specify the .wasm target like with `wasm-bindgen-cli`
+```sh
+cargo run --bin pack-wasm
+```
+
+Start the server binary from the root directory and it will server everything in the `dist` folder
+```sh
+cargo run --bin tanks_server
+```
---
+++ pack-wasm/src/main.rs
@@ -0,0 +1,73 @@
+use std::{
+    io::{self, Write},
+    process::{exit, Command, Output},
+};
+
+fn main() {
+    let wasm_lib = "tanks_wasm";
+    let wasm_target = "wasm32-unknown-unknown";
+    let wasm_path = ["target/", wasm_target, "/release/", wasm_lib, ".wasm"].join("");
+    let output_dir = "dist";
+
+    println!(
+        "\n📦 Packaging [[ {} ]] lib into '{}' directory for serving through HTTP\n",
+        wasm_lib, output_dir,
+    );
+
+    let mut command = Command::new("cargo");
+    command
+        .arg("build")
+        .args(["-p", wasm_lib])
+        .arg("--lib")
+        .args(["--target", wasm_target])
+        .arg("--release");
+
+    validate_command_result(&mut command);
+
+    let mut command = Command::new("wasm-bindgen");
+    command
+        .arg(wasm_path)
+        .args(["--out-dir", output_dir])
+        .args(["--target", "web"])
+        .arg("--typescript");
+
+    validate_command_result(&mut command);
+
+    let mut command = Command::new("cp");
+    command
+        .arg(format!("{}/index.html", wasm_lib))
+        .arg(format!("{}/", output_dir));
+
+    validate_command_result(&mut command);
+
+    println!(
+        "\n⚡ Finished packaging browser resources into '{}' directory\n",
+        output_dir
+    );
+
+    if let Ok(output) = Command::new("ls").arg(output_dir).output() {
+        println!("-------- CONTENTS --------");
+        write_all_feedback(&output);
+        println!("--------------------------");
+    }
+}
+
+fn validate_command_result(command: &mut Command) {
+    println!("EXECUTING :: ( {:?} )", command);
+    let output = command.output().expect("failed to execute");
+
+    if !output.status.success() {
+        println!("FAILURE ✖");
+        println!("---------------- FAILURE OUTPUT ----------------");
+        write_all_feedback(&output);
+        println!("------------------------------------------------");
+        exit(1);
+    } else {
+        println!("SUCCESS ✔");
+    }
+}
+
+fn write_all_feedback(output: &Output) {
+    io::stdout().write_all(&output.stdout).unwrap();
+    io::stderr().write_all(&output.stderr).unwrap();
+}
